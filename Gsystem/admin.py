f65421c775fba1ed0dc7dbd602344ed1c5c5ee70
canato3 <s1532131uf@s.chibakoudai>
2019-10-05 14:23:47 +0900

    My Django Girls app,first commit

---
+++ Gsystem/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Gsystem
+
+admin.site.register(Gsystem)
+
+# Register your models here.
---
+++ Gsystem/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GsystemConfig(AppConfig):
+    name = 'Gsystem'
